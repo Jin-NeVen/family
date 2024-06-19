c0a41d6570d9a58164ce331d5a442b63909a8298
Jin-NeVen <l.huang@ntt.com>
2024-06-19 11:46:32 +0900

    add necessary properties to User

--- app/src/main/java/com/ntt/jin/family/data/User.kt
+++ app/src/main/java/com/ntt/jin/family/data/User.kt
@@ -1,3 +1,10 @@
 package com.ntt.jin.family.data
 
-data class User(val name: String)
+import com.ntt.skyway.room.sfu.LocalSFURoomMember
+import com.ntt.skyway.room.sfu.SFURoom
+
+data class User(
+    val name: String,
+    var joinedRoom: SFURoom? = null,
+    var localSFURoomMember: LocalSFURoomMember? = null
+    )
