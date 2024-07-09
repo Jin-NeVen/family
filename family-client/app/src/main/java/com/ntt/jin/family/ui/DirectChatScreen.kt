213bb2e5fef373182f409e212e50f3fa14e029b0
Jin-NeVen <l.huang@ntt.com>
2024-07-09 17:13:36 +0900

    p2p related class refactoring

--- family-client/app/src/main/java/com/ntt/jin/family/ui/DirectChatScreen.kt
+++ family-client/app/src/main/java/com/ntt/jin/family/ui/DirectChatScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -40,22 +41,20 @@ fun DirectChatScreen(
     directChatViewModel: DirectChatViewModel = viewModel(factory = DirectChatViewModel.Factory),
     navController: NavHostController)
 {
-//    BackHandler {
-//        directChatViewModel.exit()
-//        navController.popBackStack()
-//    }
 
     var expanded by remember { mutableStateOf(false) }
     var selectedOption by remember { mutableStateOf("Select Camera") }
-    val context = LocalAppContext.current
+    val applicationContext = LocalAppContext.current.applicationContext
+    val context = LocalContext.current
     val localVideoSources = directChatViewModel.localVideoSources
     val localVideoStream = directChatViewModel.localVideoStream
     val remotedVideoStream = directChatViewModel.remoteVideoStream
     LaunchedEffect(Unit) {
-        //NOTICE if we run this in main thread, when server response gets latency, it would cause crash
-        withContext(Dispatchers.IO) {
-            directChatViewModel.chatWith(context, memberName)
-        }
+//        //NOTICE if we run this in main thread, when server response gets latency, it would cause crash
+//        withContext(Dispatchers.Default) {
+//            directChatViewModel.chatWith(context, memberName)
+//        }
+        directChatViewModel.startDirectChat(applicationContext, memberName)
     }
 
     Column(modifier = Modifier.fillMaxSize()) {
--- family-client/app/src/main/java/com/ntt/jin/family/ui/DirectChatViewModel.kt
+++ family-client/app/src/main/java/com/ntt/jin/family/ui/DirectChatViewModel.kt
@@ -12,9 +12,14 @@ import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.CreationExtras
 import com.ntt.jin.family.FamilyApplication
 import com.ntt.jin.family.data.UserRepository
+import com.ntt.skyway.core.content.Stream
+import com.ntt.skyway.core.content.local.LocalAudioStream
 import com.ntt.skyway.core.content.local.LocalVideoStream
+import com.ntt.skyway.core.content.local.source.AudioSource
 import com.ntt.skyway.core.content.local.source.CameraSource
+import com.ntt.skyway.core.content.remote.RemoteAudioStream
 import com.ntt.skyway.core.content.remote.RemoteVideoStream
+import com.ntt.skyway.room.RoomPublication
 import com.ntt.skyway.room.p2p.P2PRoom
 import com.ntt.skyway.room.member.RoomMember
 import com.ntt.skyway.room.p2p.LocalP2PRoomMember
@@ -49,16 +54,111 @@ class DirectChatViewModel(
     var localVideoStream by mutableStateOf<LocalVideoStream?>(null)
         private set
 
-    var remoteVideoStream by mutableStateOf<RemoteVideoStream?>(null)
+    var localAudioStream by mutableStateOf<LocalAudioStream?>(null)
         private set
 
+    var remoteVideoStream by mutableStateOf<RemoteVideoStream?>(null)
+        private set
+    var remoteAudioStream by mutableStateOf<RemoteAudioStream?>(null)
+        private set
+
+    var p2pRoom: P2PRoom? = null
     var localP2PRoomMember: LocalP2PRoomMember? = null
+
+    private fun captureLocalVideoSteam(context: Context) {
+        val cameraList = CameraSource.getCameras(context).toList()
+        cameraList.forEach {
+            Log.d(TAG, "camera list: $it")
+        }
+        localVideoSources = cameraList
+        val cameraOption = CameraSource.CapturingOptions(800,800)
+        CameraSource.startCapturing(context, cameraList.first(), cameraOption)
+        localVideoStream = CameraSource.createStream()
+    }
+    private fun captureLocalAudioStream() {
+        AudioSource.start()
+        localAudioStream = AudioSource.createStream()
+    }
+
+    private suspend fun createRoom() {
+        val directChatRoomName = "DirectChatRoom"
+        p2pRoom = P2PRoom.findOrCreate(directChatRoomName)
+    }
+
+    private suspend fun createLocalMember(localMemberName: String) {
+        if (p2pRoom == null) {
+            Log.d(TAG, "p2p room not created/found")
+            return
+        }
+        localP2PRoomMember = p2pRoom!!.join(RoomMember.Init(localMemberName))
+        if (localP2PRoomMember == null) {
+            Log.d(TAG, "p2p member join failed")
+        }
+    }
+
+    private suspend fun publishLocalAVStream() {
+        if (localP2PRoomMember == null) {
+            Log.d(TAG, "localP2PRoomMember is null")
+            return
+        }
+        localP2PRoomMember!!.publish(localVideoStream!!)
+        localP2PRoomMember!!.publish(localAudioStream!!)
+    }
+
+    private var streamPublishedHandler: ((publication: RoomPublication) -> Unit)? = {
+        Log.d(TAG, "stream published: ${it.id}")
+        subscribeRemoteAVStreamInternal(it)
+    }
+
+    private fun subscribeRemoteAVStreamInternal(publication: RoomPublication) {
+        viewModelScope.launch {
+            if (p2pRoom == null || localP2PRoomMember == null) {
+                return@launch
+            }
+            if (publication.publisher?.id == localP2PRoomMember!!.id) {
+                return@launch
+            }
+            Log.d(TAG, "subscribe stream: ${publication.id}")
+            val subscription = localP2PRoomMember!!.subscribe(publication)
+            subscription?.stream?.let { stream ->
+                if (stream.contentType == Stream.ContentType.VIDEO) {
+                    remoteVideoStream = subscription.stream as RemoteVideoStream
+                }
+                if (stream.contentType == Stream.ContentType.AUDIO) {
+                    remoteAudioStream = subscription.stream as RemoteAudioStream
+                }
+            }
+        }
+
+    }
+
+    private fun subscribeRemoteAVStream() {
+        if (p2pRoom == null) {
+            Log.d(TAG, "p2p room not created/found")
+            return
+        }
+        p2pRoom!!.publications.forEach { publication ->
+            subscribeRemoteAVStreamInternal(publication)
+        }
+        p2pRoom!!.onStreamPublishedHandler = streamPublishedHandler
+    }
+
+    fun startDirectChat(context: Context, memberName: String) {
+        viewModelScope.launch {
+            createRoom()
+            createLocalMember(memberName)
+            captureLocalVideoSteam(context)
+            captureLocalAudioStream()
+            publishLocalAVStream()
+            subscribeRemoteAVStream()
+        }
+    }
 
     suspend fun chatWith(context: Context, memberName: String) {
         //TODO this should not be a fixed value
         val directChatRoomName = "DirectChatRoom"
         Log.d(TAG, "chat room name: $directChatRoomName")
-        val p2pRoom = P2PRoom.findOrCreate(directChatRoomName)
+        p2pRoom = P2PRoom.findOrCreate(directChatRoomName)
         if (p2pRoom == null) {
             Log.d(TAG, "p2p room not created/found")
             return
@@ -71,21 +171,28 @@ class DirectChatViewModel(
         localP2PRoomMember!!.onLeftHandler = {
             Log.d(TAG, "${localP2PRoomMember!!.name} p2p member left")
         }
-        p2pRoom.publications.forEach { publication ->
+        p2pRoom!!.publications.forEach { publication ->
             if (publication.publisher?.id == localP2PRoomMember!!.id) {
                 return@forEach
             }
             val subscription = localP2PRoomMember!!.subscribe(publication)
-            remoteVideoStream = subscription?.stream as RemoteVideoStream
+            subscription?.stream?.let { stream ->
+                if (stream.contentType == Stream.ContentType.VIDEO) {
+                    remoteVideoStream = subscription.stream as RemoteVideoStream
+                }
+                if (stream.contentType == Stream.ContentType.AUDIO) {
+                    remoteAudioStream = subscription.stream as RemoteAudioStream
+                }
+            }
         }
-        val deviceList = CameraSource.getCameras(context).toList()
-        deviceList.forEach {
+        val cameraList = CameraSource.getCameras(context).toList()
+        cameraList.forEach {
             Log.d(TAG, "camera list: $it")
         }
-        localVideoSources = deviceList
+        localVideoSources = cameraList
         Log.d(TAG, "member joined")
         val cameraOption = CameraSource.CapturingOptions(800,800)
-        CameraSource.startCapturing(context, deviceList.first(), cameraOption)
+        CameraSource.startCapturing(context, cameraList.first(), cameraOption)
         localVideoStream = CameraSource.createStream()
 
         localP2PRoomMember!!.publish(localVideoStream!!)
@@ -99,10 +206,25 @@ class DirectChatViewModel(
         CoroutineScope(Dispatchers.IO).launch {
             Log.d(TAG, "exit directChatScreen")
             CameraSource.stopCapturing()
+            AudioSource.stop()
+            if (localP2PRoomMember != null) {
+                localP2PRoomMember!!.publications.forEach { roomPublication ->
+                    localP2PRoomMember!!.unpublish(roomPublication)
+                }
+                localP2PRoomMember!!.subscriptions.forEach { roomSubscription ->
+                    localP2PRoomMember!!.unsubscribe(roomSubscription.id)
+                }
+            }
             remoteVideoStream?.removeAllRenderer()
             remoteVideoStream?.dispose()
+            remoteVideoStream = null
+            remoteAudioStream?.dispose()
+            remoteAudioStream = null
             localVideoStream?.removeAllRenderer()
             localVideoStream?.dispose()
+            localVideoStream = null
+            localAudioStream?.dispose()
+            localAudioStream = null
             if (localP2PRoomMember != null) {
                 Log.d(TAG, "call leave room")
                 localP2PRoomMember!!.leave()
