3f4d1c4be0ca5ba24fb6ad7d487ff216c0eaff54
Jin-NeVen <l.huang@ntt.com>
2024-08-06 14:35:31 +0900

    fix auth token value

--- quick-start/app/src/main/java/com/ntt/jin/skywaycomposequickstart/MainViewModel.kt
+++ quick-start/app/src/main/java/com/ntt/jin/skywaycomposequickstart/MainViewModel.kt
@@ -23,8 +23,7 @@ class MainViewModel(val applicationContext: Context) : ViewModel() {
 
     private suspend fun initializeSkyWayContext() {
         val option = SkyWayContext.Options(
-//            authToken = "PLEASE_SET_YOUR_AUTH_TOKEN",
-            authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIzZGQ1ZDhiZC0wMGNmLTQ2MDMtYWI0MS05YjY4NTAzMjMxOWUiLCJpYXQiOjE3MjI5MTI1NzEsImV4cCI6MTcyMjk5ODk3MSwic2NvcGUiOnsiYXBwIjp7ImlkIjoiYjcyYmE3NDUtNGY4Ny00OGU1LWE4MjMtZmEzMTA3NDQwMzUwIiwidHVybiI6dHJ1ZSwiYWN0aW9ucyI6WyJyZWFkIl0sImNoYW5uZWxzIjpbeyJpZCI6IioiLCJuYW1lIjoiKiIsImFjdGlvbnMiOlsid3JpdGUiXSwibWVtYmVycyI6W3siaWQiOiIqIiwibmFtZSI6IioiLCJhY3Rpb25zIjpbIndyaXRlIl0sInB1YmxpY2F0aW9uIjp7ImFjdGlvbnMiOlsid3JpdGUiXX0sInN1YnNjcmlwdGlvbiI6eyJhY3Rpb25zIjpbIndyaXRlIl19fV0sInNmdUJvdHMiOlt7ImFjdGlvbnMiOlsid3JpdGUiXSwiZm9yd2FyZGluZ3MiOlt7ImFjdGlvbnMiOlsid3JpdGUiXX1dfV19XX19fQ.gF-G83KRuza7o91RwbqU00WQ3PyoBH80gRzyYTgo61s",
+            authToken = "PLEASE_SET_YOUR_AUTH_TOKEN",
             logLevel = Logger.LogLevel.VERBOSE
         )
         skyWayInitialized = SkyWayContext.setup(applicationContext, option) { error ->
